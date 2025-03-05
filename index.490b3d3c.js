1ed61d5b757f37a9d81b0779aed9f4890c564a06
dima <shymdima74@gmail.com>
2025-03-05 07:55:04 +0200

    make build

---
+++ index.490b3d3c.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".main__form");e&&e.addEventListener("submit",function(t){t.preventDefault(),e.reset()})});
+//# sourceMappingURL=index.490b3d3c.js.map
